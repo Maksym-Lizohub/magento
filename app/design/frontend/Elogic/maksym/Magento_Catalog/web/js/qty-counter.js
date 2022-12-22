9be553c5a9817382023c0b9f2bfcc1f06e53bc84
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-12-22 15:58:22 +0200

    after mentor first reveiw

--- app/design/frontend/Elogic/maksym/Magento_Catalog/web/js/qty-counter.js
+++ app/design/frontend/Elogic/maksym/Magento_Catalog/web/js/qty-counter.js
@@ -2,29 +2,29 @@ define(["jquery"], function ($) {
     ("use strict");
 
     $(function () {
-        $("#adds").on("click", add);
-        $("#subs").on("click", remove);
+        $("button.adds").on("click", add);
+        $("button.subs").on("click", remove);
     });
 
     function add() {
-        var input = $("input#qty"),
+        var input = $("input.qty"),
             value = input.val();
 
         input.val(++value);
 
         if (value > 0) {
-            $("#subs").removeAttr("disabled");
+            $("button.subs").removeAttr("disabled");
         }
     }
 
     function remove() {
-        var input = $("input#qty"),
+        var input = $("input.qty"),
             value = input.val();
 
         if (value > 1) {
             input.val(--value);
         } else {
-            $("#subs").attr("disabled", "disabled");
+            $("button.subs").attr("disabled", "disabled");
         }
     }
 });
